003092acedc13d04b1344b4ed0a0005b55f73ad5
Krzysztof Skwarski <krzysztof@kwartyl.pl>
2021-09-23 13:01:25 +0200

    async data fetch added to app and appropriate response handler created

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -5,44 +5,57 @@ import { PostAuthor } from './PostAuthor';
 import { ReactionButtons } from './ReactionButtons';
 import { TimeAgo } from './TimeAgo';
 import { selectAllPosts, fetchPosts } from './postsSlice';
+import { Spinner } from '../../components/Spinner';
+
+const PostExcrept = ({ post }) => {
+    return (
+        <article className="post-excerpt" key={post.id}>
+            <h3>{post.title}</h3>
+            <div>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+            </div>
+            <p className="post-content">{post.content.substring(0, 100)}</p>
+
+            <ReactionButtons post={post} />
+            <Link to={`/posts/${post.id}`} className="button muted-button">
+                View Post
+            </Link>
+        </article>
+    )
+}
 
 export const PostsList = () => {
     const dispatch = useDispatch();
     const posts = useSelector(selectAllPosts);
 
     const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
 
     useEffect(() => {
         if (postStatus === 'idle') {
             dispatch(fetchPosts())
         }
     }, [postStatus, dispatch])
-    
-    //sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt" key= {post.id}>
-            <h3>{ post.title }</h3>
-            <div>
-                <PostAuthor userId={post.user} />
-                <TimeAgo timestamp={post.date} />
-            </div>
-            <p className="post-content">
-                { post.content.substring(0, 100) }
-            </p>
-            <div>
-                <ReactionButtons post={post} />
-                <Link to={`/posts/${post.id}`} className="button muted-button">
-                    View Post
-                </Link>
-            </div>
-        </article>
-    ))
+    let content
+
+    if (postStatus === 'loading') {
+        content = <Spinner text="Loading..." />
+    } else if (postStatus === 'succeeded') {
+        //sort posts in reverse chronological order by datetime string
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => (
+            <PostExcrept key={post.id} post={post} />
+        ))
+    } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
+    }
+    
     return (
         <section className="posts-list">
             <h2>Posts</h2>
-            { renderedPosts }
+            { content }
         </section>
     )
 }
