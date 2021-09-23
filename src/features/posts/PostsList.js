b08446763fc8ea11018a5a84577311f8d1bfaaf2
Krzysztof Skwarski <krzysztof@kwartyl.pl>
2021-09-23 11:58:36 +0200

    async thun middleware added to store

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
 import { ReactionButtons } from './ReactionButtons';
 import { TimeAgo } from './TimeAgo';
+import { selectAllPosts, fetchPosts } from './postsSlice';
 
 export const PostsList = () => {
-    const posts = useSelector(state => state.posts);
+    const dispatch = useDispatch();
+    const posts = useSelector(selectAllPosts);
 
+    const postStatus = useSelector(state => state.posts.status)
+
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
+    
     //sort posts in reverse chronological order by datetime string
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
