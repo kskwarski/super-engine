684e53a324a16a8e09999077c5d793bb9d249326
Krzysztof Skwarski <krzysztof@kwartyl.pl>
2021-09-27 17:23:02 +0200

    users rendering optimized

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,26 +1,24 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, createEntityAdapter } from '@reduxjs/toolkit';
 import { client } from '../../api/client';
 
-const initialState = []
+const userAdapter = createEntityAdapter()
+
+const initialState = userAdapter.getInitialState()
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
     const response = await client.get('/fakeApi/users')
     return response.data
 })
 
-export const selectAllUsers = state => state.users
-
-export const selectUserById = (state, userId) => state.users.find(user => user.id === userId)
-
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {},
     extraReducers(bulder) {
-        bulder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload
-        })
+        bulder.addCase(fetchUsers.fulfilled, userAdapter.setAll)
     }
 })
 
 export default usersSlice.reducer;
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } = userAdapter.getSelectors(state => state.users)
